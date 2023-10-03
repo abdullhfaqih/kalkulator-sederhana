48ec73c7ae51854b5d9c65acf5f6705f4ab7f0fc
abdullah faqih <faqih7665@gmail.com>
2023-10-03 23:16:30 +0700

    first commit, new projec

---
+++ script.js
@@ -0,0 +1,43 @@
+// MENANGKAP SEMUA ELEMENT (DOM)
+const btnEnter = document.querySelector(".btn-enter");
+const perintah = document.querySelector(".command-input");
+const inputNumber = document.querySelectorAll(".input-number");
+const jumlah = document.querySelector(".input-jumlah");
+
+// KETIKA TOMBOL ENTER DI-KLIK
+btnEnter.addEventListener("click", () => {
+  // Destructuring element input angka
+  const [angkaPertama, angkaKedua] = inputNumber;
+
+  // CEK JIKA USER TIDAK MEMASUKKAN ANGKA
+  if (angkaPertama.value === "" && angkaKedua.value === "") {
+    alert("MASUKKAN ANGKA YANG BENAR TOL!!!");
+    perintah.value = "";
+  } else {
+    const result = calculate(
+      perintah.value.toLowerCase(),
+      parseInt(angkaPertama.value),
+      parseInt(angkaKedua.value)
+    );
+    // CEK JIKA USER SALAH MEMASUKKAN PERINTAH
+    result === undefined
+      ? alert("MASUKKAN PERINTAH YANG BENAR TOL!!!")
+      : (jumlah.value = result);
+  }
+  perintah.value = "";
+});
+
+calculate = (command, ...a) => {
+  switch (command) {
+    case "tambah":
+      return a.reduce((a, b) => a + b);
+    case "kurang":
+      return a.reduce((a, b) => a - b);
+    case "kali":
+      return a.reduce((a, b) => a * b);
+    case "bagi":
+      return a.reduce((a, b) => a / b);
+    default:
+      break;
+  }
+};
